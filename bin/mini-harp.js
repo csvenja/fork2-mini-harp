6a50b9dd5b97b0ad5ce36fb27a1089d5e84d1189
csvenja <c.svenjax@gmail.com>
2014-07-03 23:54:58 -0700

    Implement A Current Time Middleware

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -3,5 +3,14 @@
 var argv = require("minimist")(process.argv.slice(2));
 var createMiniHarp = require("../index"), app = createMiniHarp();
 var port = argv.port || 4000;
+
 console.log("Starting mini-harp on http://localhost:" + port);
-app.listen(port);
+app
+	.use(function (req, res, next) {
+		if (req.url === '/current-time') {
+			res.end((new Date()).toISOString() + '\n');
+		} else {
+			next();
+		}
+	})
+	.listen(port);
