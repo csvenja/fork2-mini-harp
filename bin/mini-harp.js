de33ce792960d3b7c8c52c4f2bb683b8cd3a530a
csvenja <c.svenjax@gmail.com>
2014-07-04 23:49:35 -0700

    Fix semicolons and quotes.

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var argv = require("minimist")(process.argv.slice(2));
-var createMiniHarp = require("../index")
+var createMiniHarp = require("../index");
 var path = argv._[0] || process.cwd();
 var port = argv.port || 4000;
 var app = createMiniHarp(path);
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var connect = require("connect");
 var serveStatic = require("serve-static");
 var makeJade = require("./lib/processor/jade");
+// var makeLess = require("./lib/processor/less");
 
 module.exports = function (path) {
 	var app = connect();
 	app.use(function (req, res, next) {
-		if (req.url === '/current-time') {
-			res.end((new Date()).toISOString() + '\n');
+		if (req.url === "/current-time") {
+			res.end((new Date()).toISOString() + "\n");
 		} else {
 			next();
 		}
@@ -14,4 +15,4 @@ module.exports = function (path) {
 	app.use(serveStatic(path));
 	app.use(makeJade(path));
 	return app;
-}
+};
